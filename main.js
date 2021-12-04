3e12f97e5d1811514d2f933ff1f4748ceef90d8a
Azakkiel <azakkiel@gmail.com>
2021-12-04 09:21:33 +0100

    take user input method

--- main.js
+++ main.js
@@ -8,12 +8,32 @@ const pathCharacter = '*';
 class Field {
   constructor(twoDimArray){
     this._field = twoDimArray;
+    this._posX = 0;
+    this._posY = 0;
 }
 print () {
     for (let i = 0; i <this._field.length;i++){
-        console.log(this._field[i].join());
+        console.log(this._field[i].join(' '));
     }
 }
+//takes in direction from user and moves him
+promptInput() {
+    const input = prompt('Enter a direction: N, W, E, S: ');
+    if (input == 'N'){
+        this._posY --;
+    } else if (input == 'S'){
+        this._posY ++;
+    } else if (input == 'W'){
+        this._posX --;
+    } else if (input== 'E'){
+        this._posY ++;
+    } else {
+        console.log('wrong input, try again')
+        this.promptInput();
+}
+}
+
+
 }
 
 const kkt = new Field([
@@ -21,4 +41,4 @@ const kkt = new Field([
     ['░', 'O', '░'],
     ['░', '^', '░'],
   ]);
-  kkt.print();
+  kkt.promptInput();
